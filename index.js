a89c70518565aedc3562f8f84de12c0b999ddceb
Lena Al <lena.al.developer@gmail.com>
2022-08-30 20:59:33 -0700

    first part of the game

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 let score = 0;
+
+const holes = document.getElementsByClassName('hole');
+
+//using jQuery method:
+// const holes = $('.hole');
+
+setInterval(() => {
+  const randomNumber = Math.floor(Math.random() * holes.length + 1);
+  for (let index = 0; index < holes.length; index++) {
+    $(holes[randomNumber]).toggleClass('mole');
+  }
+}, 300);
